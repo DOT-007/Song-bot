28bf13bfd56578b455c38c68a23e20c6a3503b3e
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-10-21 19:14:29 +0530

    Update song.py

--- song.py
+++ song.py
@@ -1,9 +1,9 @@
 from HorridAPI import Songmrz
 import os
 from pyrogram import Client, filters
-import aiohttp
+from aiohttp import web
 from dotenv import load_dotenv  # Import dotenv to load environment variables
-
+from Bot import web_server
 # Load environment variables from .env file
 load_dotenv()
 
@@ -17,44 +17,25 @@ API_KEY = os.getenv("API_KEY")
 if not all([TOKEN, API_ID, API_HASH, API_KEY]):
     raise ValueError("Missing environment variables. Please check your .env file.")
 
-# Initialize the bot and the API
-bot = Client(name="bot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
-Horrid = Songmrz(API_KEY)
 
-@bot.on_message(filters.command(["song", "play", "played"]))
-async def song(client, message):
-    if len(message.command) < 2:
-        await message.reply("Give Any Song Name!")
-        return
+class Bot(Client):
+    def __init__(self):
+        super().__init__(
+            name="Bot",
+            api_id=API_ID,
+            api_hash=API_HASH,
+            bot_token=TOKEN,
+            workers=200,
+            plugins={"root": "Bot"},
+            sleep_threshold=15,
+        )
 
-    query = " ".join(message.command[1:])
-    m = await message.reply_text("📥 Downloading...")
-
-    try:
-        data = Horrid.download(query)
-        url = data.url
-        thumb_url = data.thumb
-        title = data.title
-        dura = data.duration
-        songs = f"Title: {title}\nDuration: {dura}\nProvided by @Mrz_bots"
-
-        await m.edit("📤 Uploading...")
-        await message.reply_photo(photo=thumb_url, caption=songs)
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                with open(f"{title}.mp3", "wb") as f:
-                    f.write(await resp.read())  # Use await resp.read() to download the song
-            async with session.get(thumb_url) as resp:
-                with open("thumb.jpg", "wb") as f:
-                    f.write(await resp.read())  # Download the thumbnail
-
-        await message.reply_audio(f"{title}.mp3", thumb="thumb.jpg", title=title, caption=songs)
-    except Exception as e:
-        await m.edit("An error occurred while processing your request.")
-        print(f"Error: {e}")  # Log the error for debugging
-
-    await m.delete()
-
-print("Bot is working")
-bot.run()
+    async def start(self):
+        await super().start()
+        me = await self.get_me()
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        await web.TCPSite(app, "0.0.0.0", 8080).start()
+        print(f"{me.first_name} Now Working 😘")
+        
+Bot().run()
